f99bf08dc5bd0fb48e4dd6025779c3fe6b53611b
Nithikorn <nithikorn999@gmail.com>
2025-09-22 23:21:28 +0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -35,17 +35,21 @@ void CreateTriangle()
     GLfloat vertices[] =
     {
         -1.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 1.0f,
         1.0f, -1.0f, 0.0f,
         0.0f, 1.0f, 0.0f
     };
 
     unsigned int indices[] = 
-    {
+    {   
+        0, 3, 1,
+        1, 3, 2,
+        2, 3, 0,
         0, 1, 2,
     };
 
     Mesh *obj1 = new Mesh();
-    obj1->CreateMesh(vertices, indices, 9, 3);
+    obj1->CreateMesh(vertices, indices, 12, 12);
     meshList.push_back(obj1);
 }
 
@@ -64,6 +68,11 @@ int main()
     CreateTriangle();
     CreateShaders();
 
+    GLuint uniformModel = 0;
+    GLuint uniformProjection = 0;
+
+    glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
     //Loop until window closed
     while (!mainWindow.getShouldClose())
     {
@@ -76,8 +85,18 @@ int main()
 
         //draw here
         shaderList[0]->UseShader();
+        uniformModel = shaderList[0] ->GetUniformLocation("model");
+        uniformProjection = shaderList[0] ->GetUniformLocation("projection");
 
         //Object
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
+        // model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4, 0.4f, 1.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+
         meshList[0]->RenderMesh();
 
         glUseProgram(0);
