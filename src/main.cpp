b33b5ce6d371af070ee83cb3d6373c9907f0c19e
Nithikorn <nithikorn999@gmail.com>
2025-09-23 01:22:19 +0700

    update shader and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,10 @@ void CreateTriangle()
 
     Mesh *obj1 = new Mesh();
     obj1->CreateMesh(vertices, indices, 12, 12);
-    meshList.push_back(obj1);
+
+    for (int i = 0; i < 10; i++) {
+        meshList.push_back(obj1);
+    }
 }
 
 void CreateShaders()
@@ -69,9 +72,12 @@ int main()
     CreateShaders();
 
     GLuint uniformModel = 0;
+    GLuint uniformView = 0;
     GLuint uniformProjection = 0;
 
     glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
+    // glm::mat4 projection = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, 0.1f, 100.0f);
 
     //Loop until window closed
     while (!mainWindow.getShouldClose())
@@ -86,18 +92,58 @@ int main()
         //draw here
         shaderList[0]->UseShader();
         uniformModel = shaderList[0] ->GetUniformLocation("model");
+        uniformView = shaderList[0] ->GetUniformLocation("view");
         uniformProjection = shaderList[0] ->GetUniformLocation("projection");
 
+        glm::mat4 view(1.0f);
+
+        glm::vec3 cameraPos = glm::vec3(1.0f, 0.5f, 2.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, -0.3f, -1.0f);
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        glm::vec3 cameraDirection = glm::normalize(cameraTarget - cameraPos);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraDirection, up));
+        glm::vec3 cameraUp = glm::normalize(glm::cross(cameraRight, cameraDirection));
+
+        // glm::mat4 cameraPosMat (1.0f);
+        // cameraPosMat[3][0] = -cameraPos.x;
+        // cameraPosMat[3][1] = -cameraPos.y;
+        // cameraPosMat[3][2] = -cameraPos.z;
+
+        // glm::mat4 cameraRotateMat (1.0f);
+        // cameraRotateMat[0] = glm::vec4(cameraRight.x, cameraUp.x, -cameraDirection.x, 0.0f);
+        // cameraRotateMat[1] = glm::vec4(cameraRight.y, cameraUp.y, -cameraDirection.y, 0.0f);
+        // cameraRotateMat[2] = glm::vec4(cameraRight.z, cameraUp.z, -cameraDirection.z, 0.0f);
+
+        // view = cameraRotateMat * cameraPosMat;
+
+        view = glm::lookAt(cameraPos, cameraPos + cameraDirection, cameraUp);
         //Object
-        glm::mat4 model(1.0f);
-        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
-        // model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        model = glm::scale(model, glm::vec3(0.4, 0.4f, 1.0f));
+        glm::vec3 pyramidPositions[] = {
+            glm::vec3(0.0f, 0.0f, -2.5f),
+            glm::vec3(2.0f, 5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3(2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f, 3.0f, -7.5f),
+            glm::vec3(1.3f, -2.0f, -2.5f),
+            glm::vec3(1.5f, 2.0f, -2.5f),
+            glm::vec3(1.5f, 0.2f, -1.5f),
+            glm::vec3(-1.3f, 1.0f, -1.5f)
+        };
+        
+        for (int i = 0; i < 10; i++) {
+            glm::mat4 model(1.0f);
+            model = glm::translate(model, pyramidPositions[i]);
+            model = glm::rotate(model, glm::radians(2.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::scale(model, glm::vec3(0.8, 0.8f, 1.0f));
 
-        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+            glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(view));
+            glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
-        meshList[0]->RenderMesh();
+            meshList[i]->RenderMesh();
+        }
 
         glUseProgram(0);
         //end draw
